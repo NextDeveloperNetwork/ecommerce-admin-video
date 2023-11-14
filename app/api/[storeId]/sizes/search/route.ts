c062a56393d3af2f793bc316b0e79e98b2bab049
BlackKnightNET <g3albania.elvis@gmail.com>
2023-11-14 08:51:33 +0100

    finalAS0.0.0.1

---
+++ app/api/[storeId]/sizes/search/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server';
+
+import prismadb from '@/lib/prismadb';
+
+export async function GET(
+    req: Request,
+    { params }: {params: { storeId: string }}
+  ) {
+    try {
+
+        const { searchParams } = new URL(req.url);
+        const searchValue = decodeURIComponent(searchParams.get('searchValue') || "") || undefined;
+      
+        if (!params.storeId) {
+            return new NextResponse("StoreId is required", { status: 400 });
+        }
+  
+        const sizes = await prismadb.size.findMany({
+            where: {
+                storeId: params.storeId,
+                products: {
+                    some: {
+                        name: {
+                            contains: searchValue
+                        }
+                    }
+                }
+            },
+        });
+        console.log('Search:' + searchValue)
+        return NextResponse.json(sizes);
+
+    } catch (error) {
+        console.log('[SEARCH_SIZES_GET]', error);
+        return new NextResponse("Internal error", { status: 500 });
+    }
+};
