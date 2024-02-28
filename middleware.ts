6b32abdeafe473eea7bb3ea0ffd559f5cdd5cd25
BlackKnightNET <g3albania.elvis@gmail.com>
2024-02-28 10:34:23 +0100

    final_B4_A_0.0.2.7

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/api/:path*", "/api/webhooks(.*)"],
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/api/webhook/user"]
 });
 
 export const config = {
