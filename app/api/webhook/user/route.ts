d8116b8eef07b1096494391cabcfc2f8b75c533f
BlackKnightNET <g3albania.elvis@gmail.com>
2024-03-03 15:37:48 +0100

    final_B4_A_0.0.3.0

--- app/api/webhook/user/route.ts
+++ app/api/webhook/user/route.ts
@@ -5,38 +5,40 @@ import {headers}from "next/headers";
 import {Webhook, WebhookRequiredHeaders}from "svix"
 
 const webhookSecret=process.env.WEBHOOK_SECRET ||"";
+async function handler (request:Request){
+const payload=await request.json();
+const headersList=headers();
+const heads={
+    "svix-id": headersList.get("svix-id"),
+    "svix-timestamp": headersList.get("svix-timestamp"),
+    "svix-signature": headersList.get("svix-signature"),
+}
+const wh =new Webhook(webhookSecret)
+let evt:Event|null=null;
+try {
+    evt =wh.verify(JSON.stringify(payload),
+    heads as IncomingHttpHeaders & WebhookRequiredHeaders
+    )as Event;
+} catch (error) {
+    console.error((error as Error).message)
 
-async function handler(request: Request) {
-    const payload = await request.json();
-    const heads = {
-        "svix-id": request.headers.get("svix-id"),
-        "svix-timestamp": request.headers.get("svix-timestamp"),
-        "svix-signature": request.headers.get("svix-signature"),
-    }
-    const wh = new Webhook(webhookSecret)
-    let evt: Event | null = null;
-    try {
-        evt = wh.verify(JSON.stringify(payload), heads as IncomingHttpHeaders & WebhookRequiredHeaders) as Event;
-    } catch (error) {
-        console.error((error as Error).message)
-        return NextResponse.json({}, { status: 400 })
-    }
-    const eventType: EventType = evt.type;
-    if (eventType === "user.created" || eventType === "user.updated") {
-        const { id, ...attributes } = evt.data;
-
-        await prismadb.user.upsert({
-            where: { externalId: id as string },
-            create: {
-                externalId: id as string,
-                attributes
-            },
-            update: { attributes }
-        })
-    }
+    return NextResponse.json({},{status:400})
+}
+const eventType: EventType=evt.type;
+if (eventType==="user.created"|| eventType === "user.updated"){
+    const {id, ...attributes}=evt.data;
+    
+    await prismadb.user.upsert({ 
+        where:{externalId:id as string},
+        create:{
+            externalId: id as string,
+            attributes
+        },
+        update:{attributes}
+    })
 }
 
-
+}
 type EventType="user.created"|"user.updated"|"#";
 type Event={
     data:Record<string, string|number>,
